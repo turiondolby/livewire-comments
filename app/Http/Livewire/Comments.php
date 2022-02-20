9f0cbc48edbc2a873d9031d533bdb78c6ef64fd3
Haleem Hosein <haleemhosein@yahoo.com>
2022-02-20 13:41:11 -0400

    01 Into + Demo  & 02 Laravel, Livewire and Breeze setup

---
+++ app/Http/Livewire/Comments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Comments extends Component
+{
+    public function render()
+    {
+        return view('livewire.comments');
+    }
+}
