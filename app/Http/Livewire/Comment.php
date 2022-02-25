c834a2a57aa7c45d043c58a3be64f30e8b14ac70
Haleem Hosein <haleemhosein@yahoo.com>
2022-02-25 19:43:31 -0400

    09 Toggling reply state -- add state for reply comment

--- app/Http/Livewire/Comment.php
+++ app/Http/Livewire/Comment.php
@@ -8,6 +8,17 @@ class Comment extends Component
 {
     public $comment;
 
+    public $isReplying = false;
+
+    public $replyState = [
+        'body' => ''
+    ];
+
+    public function postReply()
+    {
+        dd($this->replyState);
+    }
+
     public function render()
     {
         return view('livewire.comment');
--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -14,22 +14,45 @@
                 <span class="text-gray-500 font-medium">
                     {{ $comment->created_at->diffForHumans() }}
                 </span>
-                @if (! $comment->parent_id)
+                @auth
+                    @if (! $comment->parent_id)
+                        <button wire:click="$toggle('isReplying')" type="button" class="text-gray-900 font-medium">
+                            Reply
+                        </button>
+                    @endif
                     <button type="button" class="text-gray-900 font-medium">
-                        Reply
+                        Edit
                     </button>
-                @endif
-                <button type="button" class="text-gray-900 font-medium">
-                    Edit
-                </button>
-                <button type="button" class="text-gray-900 font-medium">
-                    Delete
-                </button>
+                    <button type="button" class="text-gray-900 font-medium">
+                        Delete
+                    </button>
+                @endauth
             </div>
         </div>
     </div>
 
     <div class="ml-14 mt-6">
+        @if ($isReplying)
+            <form wire:submit.prevent="postReply">
+                <div>
+                    <label for="comment" class="sr-only">Reply body</label>
+                    <textarea id="comment" name="comment" rows="3"
+                              class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md
+                                        @error('replyState.body') border-red-500 @enderror"
+                              placeholder="Write something" wire:model.defer="replyState.body"></textarea>
+                    @error('replyState.body')
+                    <p class="mt-2 text-sm text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mt-3 flex items-center justify-between">
+                    <button type="submit"
+                            class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                        Comment
+                    </button>
+                </div>
+            </form>
+        @endif
+
         @foreach ($comment->children as $child)
             <livewire:comment :comment="$child" :key="$child->id"/>
         @endforeach
